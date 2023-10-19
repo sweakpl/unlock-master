8f574f88546bb0812e56d82f314992b44de84579
sweak <sweakpolska@gmail.com>
2023-10-19 17:11:45 +0200

    Fixed wrong SettingEntry separator color in dark mode

--- app/src/main/java/com/sweak/unlockmaster/presentation/settings/components/SettingsEntry.kt
+++ app/src/main/java/com/sweak/unlockmaster/presentation/settings/components/SettingsEntry.kt
@@ -10,15 +10,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.NavigateNext
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.NavigateNext
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -58,7 +57,7 @@ fun SettingsEntry(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(1.dp)
-                .background(color = Color.Black)
+                .background(color = MaterialTheme.colorScheme.onBackground)
         )
     }
 }
