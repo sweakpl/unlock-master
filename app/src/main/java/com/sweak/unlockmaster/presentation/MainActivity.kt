e85c0bd11285e6153d9187e402a5dab8ae727fd6
sweak <hubert.tacik@proton.me>
2024-05-26 11:54:23 +0200

    Updated dependencies + upgraded AGP to 8.4.1

--- app/src/main/java/com/sweak/unlockmaster/presentation/MainActivity.kt
+++ app/src/main/java/com/sweak/unlockmaster/presentation/MainActivity.kt
@@ -298,7 +298,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    override fun onNewIntent(intent: Intent?) {
+    override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         setIntent(intent)
     }
