1052d63d8e1e3c341b5d8c71c391102c2cce4c58
sweak <hubert.tacik@proton.me>
2024-09-28 01:26:07 +0200

    Now handling the ACTION_BOOT_COMPLETED being delivered after force-close

--- app/src/main/java/com/sweak/unlockmaster/presentation/background_work/global_receivers/BootReceiver.kt
+++ app/src/main/java/com/sweak/unlockmaster/presentation/background_work/global_receivers/BootReceiver.kt
@@ -1,9 +1,11 @@
 package com.sweak.unlockmaster.presentation.background_work.global_receivers
 
+import android.app.ActivityManager
 import android.app.KeyguardManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import com.sweak.unlockmaster.domain.repository.UserSessionRepository
 import com.sweak.unlockmaster.domain.use_case.daily_wrap_up.ScheduleDailyWrapUpNotificationsUseCase
 import com.sweak.unlockmaster.domain.use_case.screen_on_events.AddScreenOnEventUseCase
@@ -40,6 +42,17 @@ class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action in intentActionsToFilter) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+                val activityManager =
+                    context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+
+                activityManager.getHistoricalProcessStartReasons(1).firstOrNull()?.let {
+                    // On Android 15+ ACTION_BOOT_COMPLETED is delivered on post-force-close launch.
+                    // We detect that and do not act as if an actual boot happened and just return:
+                    if (it.wasForceStopped()) return
+                }
+            }
+
             runBlocking {
                 if (userSessionRepository.isIntroductionFinished() &&
                     !keyguardManager.isKeyguardLocked &&
